34329437b1a8f001cf564bdab065ad7876d85336
Reagan McFarland <reagan.mcfarland1059@gmail.com>
2021-04-02 11:35:16 -0400

    [ REMOVED ] removed unused entry file cli param
    Not implemented yet, and won't be for a while so removed

--- src/cli.rs
+++ src/cli.rs
@@ -8,8 +8,8 @@ pub enum SupportedLanguage {
 impl std::fmt::Display for SupportedLanguage {
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
     match self {
-      &SupportedLanguage::RUST => write!(f, "rust"),
-      &SupportedLanguage::NODE => write!(f, "node") 
+      SupportedLanguage::RUST => write!(f, "rust"),
+      SupportedLanguage::NODE => write!(f, "node") 
     }
   }
 }
@@ -18,7 +18,6 @@ pub struct Cli {
   pub watch_patterns: Vec<String>, // file patterns to watch
   pub project_language: SupportedLanguage,
   pub exec_command: String, // subcommand that was used for
-  pub entry_file: Option<String>, // entry file, only applicable for particular configuraitons
 }
 
 impl Cli {
@@ -29,7 +28,6 @@ impl Cli {
     let mut watch_patterns: Vec<String> = Vec::new();
     let project_language: SupportedLanguage;
     let exec_command: String;
-    let entry_file: Option<String> = None;
     
     if matches.is_present("rust") {
       watch_patterns.push("*.rs".to_string());
@@ -46,6 +44,6 @@ impl Cli {
       std::process::exit(1);
     }
     
-    Cli { watch_patterns, project_language, exec_command, entry_file }
+    Cli { watch_patterns, project_language, exec_command }
   }
 }
