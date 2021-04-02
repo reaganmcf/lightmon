e55157002ccfa33bdf3a3284ac781eb2c978b2c9
Reagan McFarland <reagan.mcfarland1059@gmail.com>
2021-04-02 11:35:56 -0400

    [ ADD ] watcher reads watch_patterns cli arg
    Watcher watches file according to the patterns sent in. However, we need to restructure our program to have a cmd thread that runs the exec commands, and the watcher thread. this way we aren't starting neseted processes infinitely

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,9 @@ use cli::Cli;
 fn main() {
   let cli_args: Cli = Cli::new();
   println!("lightmon started ({} mode)", cli_args.project_language);
-  println!("Parsed / configured args: {:?}", (cli_args.watch_patterns, cli_args.exec_command, cli_args.entry_file));
+  //println!("Parsed / configured args: {:?}", (cli_args.watch_patterns, cli_args.exec_command));
   
-  watcher::start();
+  watcher::start(cli_args);
   
 }
 
--- src/watcher.rs
+++ src/watcher.rs
@@ -2,38 +2,65 @@ extern crate notify;
 extern crate walkdir;
 
 use notify::{Watcher, RecursiveMode, watcher};
-use std::sync::mpsc::channel;
+use std::{process::Command, sync::mpsc::channel};
 use std::time::Duration;
 use walkdir::WalkDir;
+use std::collections::HashSet;
 
+pub use crate::cli::Cli; 
 
-
-
-pub fn start() {
+pub fn start(cli_args: Cli) {
     // Create a channel to receive the events.
     let (tx, rx) = channel();
 
     // Create a watcher object, delivering debounced events.
     // The notification back-end is selected based on the platform.
-    let mut watcher = watcher(tx, Duration::from_secs(10)).unwrap();
+    let mut watcher = watcher(tx, Duration::from_millis(100)).unwrap();
 
-
-    for entry in WalkDir::new(".")
-            .follow_links(true)
-            .into_iter()
-            .filter_map(|e| e.ok()) {
-        let f_name = entry.file_name().to_string_lossy();
-
-        if f_name.ends_with(".rs") {
-            watcher.watch(entry.path().to_str().unwrap(),RecursiveMode::NonRecursive).unwrap();
-            // println!("f_name = {:?}", entry.path().to_str().unwrap());
-        }
+    let mut explicit_files_to_watch: HashSet<String> = HashSet::new();
+    let mut file_types_to_watch: HashSet<String> = HashSet::new();
+  
+    for pattern in cli_args.watch_patterns {
+      // *.xxx pattern
+      if pattern.starts_with("*.") {
+        file_types_to_watch.insert(pattern[pattern.find(".").unwrap()..pattern.len()].to_string());
+      } else {
+        explicit_files_to_watch.insert(pattern.to_string());
+      }
     }
 
+    for entry in WalkDir::new(".").follow_links(true).into_iter().filter_map(|e| e.ok()) {
+      let mut should_watch: bool;
+      // Check if the file is an explicit one we should watch
+      should_watch = explicit_files_to_watch.contains(entry.path().to_str().unwrap());
+      // Check if the file ends in a type we should watch
+      let f_name = entry.file_name().to_string_lossy();
+      should_watch = should_watch || f_name.ends_with(".rs");
+      
+      if should_watch {
+        println!("Started watch on {:?}", entry.path().to_str().unwrap());
+        watcher.watch(entry.path().to_str().unwrap(),RecursiveMode::NonRecursive).unwrap();
+      }
+    }
+
+    // TODO use cli args instead
+    let mut exec_cmd = Command::new("cargo");
+    exec_cmd.arg("build");
+    
     loop {
-        match rx.recv() {
-           Ok(event) => println!("{:?}", event),
-           Err(e) => println!("watch error: {:?}", e),
+      match rx.recv() {
+       Ok(event) => {
+        println!("Changes detected... Restarting using {:?}", exec_cmd);
+        let res = exec_cmd.spawn();
+        if let Ok(child) = res {
+          println!("Started child {:?}", child);
+        } else {
+          println!("FAILED {:?}", res.err());
         }
+       },
+       Err(e) => {
+         println!("watch error: {:?}", e);
+       }
+      }
     }
 }
