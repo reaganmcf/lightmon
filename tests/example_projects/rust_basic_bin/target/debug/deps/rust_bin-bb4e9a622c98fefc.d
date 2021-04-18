4f918cee71b0d3e78598f7957e360269246b1d36
Reagan McFarland <reagan.mcfarland1059@gmail.com>
2021-04-18 12:25:31 -0400

    :fire: Gitignore includes example project junk
    Removed junk generated from the test suite for the example projects from source control and updates the gitignore/

--- tests/example_projects/rust_basic_bin/target/debug/deps/rust_bin-bb4e9a622c98fefc.d
+++
@@ -1,5 +0,0 @@
-/home/rmcf/Code/lightmon/tests/example_projects/rust_basic_bin/target/debug/deps/rust_bin-bb4e9a622c98fefc: src/main.rs
-
-/home/rmcf/Code/lightmon/tests/example_projects/rust_basic_bin/target/debug/deps/rust_bin-bb4e9a622c98fefc.d: src/main.rs
-
-src/main.rs:
--- tests/example_projects/rust_basic_lib/target/debug/deps/rust_lib-7dc1d9941c0ba5f6.d
+++
@@ -1,5 +0,0 @@
-/home/rmcf/Code/lightmon/tests/example_projects/rust_basic_lib/target/debug/deps/rust_lib-7dc1d9941c0ba5f6: src/lib.rs
-
-/home/rmcf/Code/lightmon/tests/example_projects/rust_basic_lib/target/debug/deps/rust_lib-7dc1d9941c0ba5f6.d: src/lib.rs
-
-src/lib.rs:
