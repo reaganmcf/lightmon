0e5b774859aaedf1049e35842a545ef72af73554
Reagan McFarland <reagan.mcfarland1059@gmail.com>
2021-05-17 19:51:47 -0400

    fix: Kill previous exec thread before spawning new
    Before this fix, previous exec threads would not be killed between
    restarts. This means the process would pile up over time and that is
    definitely not a good thing.
    
    However, with this patch, the exec thread sends the Child handles to the
    main event loop through a new channel where they get dealt with before
    the new exec thread is spawned to take it's place.

--- src/main.rs
+++ src/main.rs
@@ -101,14 +101,16 @@ mod exec;
 mod watcher;
 
 use cli::Cli;
-use std::sync::mpsc::channel;
 use std::sync::Arc;
+use std::{
+    process::Child,
+    sync::mpsc::{channel, Receiver, Sender},
+};
 
 /// Type of events that lightmon handles.
 pub enum LightmonEvent {
     /// When lightmon first starts up successfully, it forces the exec thread to go off once
     InitExec,
-    KillChild,
     KillAndRestartChild,
 }
 
@@ -118,6 +120,10 @@ fn main() {
 
     // get ligthmon event channel
     let (lightmon_event_sender, lightmon_event_receiver) = channel();
+
+    // exec children references channel. Used to kill already running exec threads
+    let (exec_child_process_sender, exec_child_process_receiver): (Sender<Child>, Receiver<Child>) =
+        channel();
 
     // Send first dummy event
     match lightmon_event_sender.send(LightmonEvent::InitExec) {
@@ -137,14 +143,39 @@ fn main() {
             match lightmon_event {
                 LightmonEvent::KillAndRestartChild => {
                     debug!("KILL AND RESTART RECEIEVED");
-                    exec::start(cli_args.clone(), lightmon_event_sender.clone());
-                }
-                LightmonEvent::KillChild => {
-                    debug!("KILL RECEIEVED");
+
+                    // kill child
+                    if let Ok(mut exec_child) = exec_child_process_receiver.recv() {
+                        match exec_child.kill() {
+                            Ok(_) => debug!("Killed child process."),
+                            Err(e) => debug!("Failed to kill child process! {:?}", e),
+                        }
+
+                        // waiting after killing sounds weird because it is. But, the following is
+                        // from the rust doc:
+                        //
+                        // On some systems, calling wait or similar is necessary for the OS to release resources.
+                        // A process that terminated but has not been waited on is still around as a “zombie”.
+                        // Leaving too many zombies around may exhaust global resources (for example process IDs).
+                        // The standard library does not automatically wait on child processes (not even if the Child is dropped), it is up to the application developer to do so.
+                        // As a consequence, dropping Child handles without waiting on them first is not recommended in long-running applications.
+                        let _ = exec_child.try_wait();
+                    }
+
+                    // Restart
+                    exec::start(
+                        cli_args.clone(),
+                        lightmon_event_sender.clone(),
+                        exec_child_process_sender.clone(),
+                    );
                 }
                 LightmonEvent::InitExec => {
                     debug!("INIT EXEC RECEIVED");
-                    exec::start(cli_args.clone(), lightmon_event_sender.clone());
+                    exec::start(
+                        cli_args.clone(),
+                        lightmon_event_sender.clone(),
+                        exec_child_process_sender.clone(),
+                    );
                 }
             }
         }
