776efc4d892fb4dc5f6b70f124448ee5f9250884
Reagan McFarland <reagan.mcfarland1059@gmail.com>
2021-05-25 13:18:07 -0400

    fix: Kill previous exec thread before spawning new (#35)
    Before this fix, previous exec threads would not be killed between
    restarts. This means the process would pile up over time and that is
    definitely not a good thing.
    
    However, with this patch, the exec thread sends the Child handles to the
    main event loop through a new channel where they get dealt with before
    the new exec thread is spawned to take it's place.

--- src/exec.rs
+++ src/exec.rs
@@ -1,10 +1,13 @@
 //! Contains the method for starting a thread that will run the exec commands in parallel.
 
-use std::io::{self};
 use std::process::Command;
 use std::sync::mpsc::Sender;
 use std::sync::Arc;
 use std::thread;
+use std::{
+    io::{self},
+    process::Child,
+};
 
 pub use crate::cli::Cli;
 pub use crate::LightmonEvent;
@@ -15,6 +18,7 @@ pub use crate::LightmonEvent;
 pub fn start(
     cli_args: Arc<Cli>,
     lightmon_event_sender: Sender<LightmonEvent>,
+    exec_child_process_sender: Sender<Child>,
 ) -> std::thread::JoinHandle<()> {
     thread::spawn(move || {
         debug!("thread started");
@@ -28,27 +32,27 @@ pub fn start(
                 cmd.arg(argument);
             }
             debug!("final cmd = {:?}", cmd);
-            let mut child = cmd.spawn().unwrap();
+            let child = cmd.spawn().unwrap();
+            debug!("child process pid = {:?}", child.id());
+            match exec_child_process_sender.send(child) {
+                Ok(_) => {}
+                Err(_) => {
+                    error!("Unable to send event to main loop. Something seriously went wrong!");
+                    std::process::exit(1);
+                }
+            }
             loop {
                 let mut input = String::new();
                 if let Ok(n) = io::stdin().read_line(&mut input) {
                     if input.eq("rs\n") {
                         debug!("rs RECEIEVED");
                         match lightmon_event_sender.send(LightmonEvent::KillAndRestartChild) {
-                            Ok(()) => {
-                                let child_killed = child.kill();
-                                match child_killed {
-                                    Ok(()) => {}
-                                    Err(e) => {
-                                        error!("program cannot be quit and restarted: {:?}", e);
-                                        std::process::exit(1);
-                                    }
-                                }
-                            }
+                            Ok(_) => {}
                             Err(_) => {
-                                panic!("failed to send initial lightmon event. Something went seriously wrong!");
+                                error!("Unable to kill and restart the process. Something seriously went wrong!");
+                                std::process::exit(1);
                             }
-                        };
+                        }
                     } else {
                         debug!("unknown input, bits read from input {:?}", n);
                         debug!("input = {:?}", input);
